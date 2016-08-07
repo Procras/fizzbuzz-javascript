17e8b2a458a7ff7656a3096d5443f671664c3258
Giancarlo <gfabrizi@gmail.com>
2016-08-07 11:28:23 +0200

    kata 2

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,42 +1,61 @@
-function FizzBuzz() {
-}
-
-FizzBuzz.prototype.play = function(number) {
-  if (this.isDivisibleBy(15, number)) {
-    return 'fizzbuzz';
-  }
-  if (this.isDivisibleBy(5, number)) {
-    return 'buzz';
-  }
-  if (this.isDivisibleBy(3, number)) {
-    return 'fizz';
-  }
-  return number;
-}
-
-FizzBuzz.prototype.isDivisibleBy = function(divisor, number) {
-  return number % divisor === 0;
-}
-
-
+// function FizzBuzz() {
+// }
+//
+// FizzBuzz.prototype.play = function(number) {
+//   if (this.isDivisibleBy(15, number)) {
+//     return 'fizzbuzz';
+//   }
+//   if (this.isDivisibleBy(5, number)) {
+//     return 'buzz';
+//   }
+//   if (this.isDivisibleBy(3, number)) {
+//     return 'fizz';
+//   }
+//   return number;
+// }
+//
+// FizzBuzz.prototype.isDivisibleBy = function(divisor, number) {
+//   return number % divisor === 0;
+// }
 
   // function FizzBuzz() {
   //
   // }
   //
-  // Fizzbuzz.prototype.play = function(n) {
-  //   if (this.isDivisibleBy(15, n)) {
+  // FizzBuzz.prototype.play = function(number) {
+  //   if (this.isDivisibleBy(15, number)) {
   //     return 'fizzbuzz';
   //   }
-  //   if (this.isDivisibleBy(5, n)) {
+  //   if(this.isDivisibleBy(5, number)) {
   //     return 'buzz';
   //   }
-  //   if (this.isDivisibleBy(3, n)) {
+  //   if(this.isDivisibleBy(3, number)) {
   //     return 'fizz';
   //   }
   //   return number;
   // }
   //
-  // FizzBuzz.prototype.isDivisibleBy = function(divisor, n) {
-  //   return n % divisor === 0
+  // FizzBuzz.prototype.isDivisibleBy = function(divisor, number) {
+  //   return number % divisor === 0
   // }
+
+    function FizzBuzz() {
+
+    }
+
+    FizzBuzz.prototype.isDivisibleBy = function(divisor, number) {
+      return number % divisor === 0
+    }
+
+    FizzBuzz.prototype.play = function(number) {
+      if (this.isDivisibleBy(15, number)) {
+        return 'fizzbuzz';
+      }
+      if (this.isDivisibleBy(5, number)) {
+        return 'buzz';
+      }
+      if (this.isDivisibleBy(3, number)) {
+        return 'fizz';
+      }
+      return number;
+    }
