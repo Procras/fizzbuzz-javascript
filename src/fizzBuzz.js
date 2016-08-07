26203b16fd1a0ce38a6e09ef13dd2730e0290c76
Giancarlo <gfabrizi@gmail.com>
2016-08-07 12:01:31 +0200

    kata 4

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -60,6 +60,27 @@
     //   return number;
     // }
 
+      // function FizzBuzz() {
+      //
+      // }
+      //
+      // FizzBuzz.prototype.isDivisibleBy = function(divisor, number) {
+      //   return number % divisor === 0
+      // }
+      //
+      // FizzBuzz.prototype.play = function(number) {
+      //   if (this.isDivisibleBy(15, number)) {
+      //     return 'fizzbuzz';
+      //   }
+      //   if (this.isDivisibleBy(5, number)) {
+      //     return 'buzz';
+      //   }
+      //   if (this.isDivisibleBy(3, number)) {
+      //     return 'fizz';
+      //   }
+      //   return number;
+      // }
+
       function FizzBuzz() {
 
       }
